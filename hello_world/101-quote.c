426c4c7c0b8bf3cdc6e8e236794b8c475e4d6fcf
Teheremiti <t.tuiaiho@gmail.com>
2023-02-15 14:07:04 -0800

    Print to the standard error

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - Print a message to the standard error
+ * Return: 1
+ */
+
+int main(void) {
+	fprintf(stderr, "%s", "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
