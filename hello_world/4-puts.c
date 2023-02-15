e4ad2097ae11fbfab32f4f7549cc40d793b30f36
Teheremiti <t.tuiaiho@gmail.com>
2023-02-15 05:14:01 -0800

    Print a string with puts function

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
 
 /*
- * main - Print message
- * End with the value 0
+ * main - Print message using puts
+ * End with value 0
  */
 
 int main(void)
 {
-	puts ("\"Programming is like building a multilingual puzzle");
+	puts "\"Programming is like building a multilingual puzzle";
 	return (0);
 }
