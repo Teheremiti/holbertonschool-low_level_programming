840b42e86134b272e34b99511c77b8950b9a9f1f
Teheremiti <t.tuiaiho@gmail.com>
2023-02-21 08:08:52 -0800

    Print last digit of a signed number

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include <time.h>
+#include "main.h"
+
+/**
+ * print_last_digit - Print last digit of a number
+ * @n: Input int
+ * Return: Value of the last digit
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+		n *= -1;
+
+	_putchar('0' + n % 10);
+	return (n % 10);
+}
