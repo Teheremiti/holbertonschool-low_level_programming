b2bd4d0e131ad0b834a712a7b73846a77365cd17
Teheremiti <t.tuiaiho@gmail.com>
2023-02-21 05:03:02 -0800

    Check if parameter is lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include <time.h>
+#include "main.h"
+
+/**
+ * _islower - Check if c is lowercase
+ * @c: ASCII value of a char (type int)
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+
+	return (0);
+}
