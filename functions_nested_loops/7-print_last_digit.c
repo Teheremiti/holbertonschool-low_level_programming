4313ca72a858a49e6a1a9ebcb68c94d0e279b411
Teheremiti <t.tuiaiho@gmail.com>
2023-02-22 00:19:22 -0800

    Add INT_MIN particularity

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -12,8 +12,12 @@ int print_last_digit(int n)
 	if (n < 0)
 		n = -n;
 
+	/**
+	 * INT_MIN = -2147483648 and INT_MAX = 2147483647 : can not be
+	 * reached by taking opposite
+	 */
 	if (n == INT_MIN)
-		n = -n - 1;
+		n = 8;
 
 	_putchar('0' + n % 10);
 	return (n % 10);
