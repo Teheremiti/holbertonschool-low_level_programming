875f0085f7f2447422028bb69f6c15e2eeedf2d7
Teheremiti <t.tuiaiho@gmail.com>
2023-02-22 00:33:29 -0800

    Print the 9 times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * times_table - Print the 9 times table
+ * Return: void
+ */
+
+void times_table(void)
+{
+	int n = 0;
+
+	for (; n <= 9; n++)
+	{
+		int k = 1;
+
+		_putchar('0');
+		_putchar(',');
+
+		for (; k <= 9; k++)
+		{
+			int multiple = n * k;
+
+			if (multiple <= 9)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + multiple);
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar('0' + multiple / 10);
+				_putchar('0' + multiple % 10);
+			}
+
+			_putchar(',');
+		}
+
+		_putchar('\n');
+	}
+}
