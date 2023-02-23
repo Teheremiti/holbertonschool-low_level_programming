e4e17af37fad3d20b1a324fad9891f0eb443a6de
Teheremiti <t.tuiaiho@gmail.com>
2023-02-23 00:31:15 -0800

    FizzBuzz test - MAJ

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,10 @@ void fizz_buzz(void)
 
 	for (; n <= 100; n++)
 	{
-		if (n % 3 == 0)
+		if (n % 3 == 0 && n % 5 == 0)
+                        printf("FizzBuzz ");
+
+		else if (n % 3 == 0)
 			printf("Fizz ");
 
 		else if (n % 5 == 0)
@@ -22,9 +25,6 @@ void fizz_buzz(void)
 			else
 				printf("Buzz ");
 		}
-
-		else if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz ");
 
 		else
 			printf("%d ", n);
