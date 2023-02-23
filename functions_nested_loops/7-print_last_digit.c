3a0ae320ef92761674e32669d177a25778a538ef
Teheremiti <t.tuiaiho@gmail.com>
2023-02-23 00:46:57 -0800

    Print last digit of a signed number - Optimization

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -9,16 +9,11 @@
 
 int print_last_digit(int n)
 {
-	if (n < 0)
-		n = -n;
+	int modulus = n % 10;
 
-	/**
-	 * INT_MIN = -2147483648 and INT_MAX = 2147483647 : can not be
-	 * reached by taking opposite
-	 */
-	if (n == INT_MIN)
-		n = 8;
+	if (modulus < 0)
+		modulus *= -1;
 
-	_putchar('0' + n % 10);
-	return (n % 10);
+	_putchar('0' + modulus);
+	return (modulus);
 }
