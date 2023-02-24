a31e14ef3c64212deab9bbeaa308702767ebd95b
Teheremiti <t.tuiaiho@gmail.com>
2023-02-24 02:11:25 -0800

    Print an integer with recursion - Fix INT_MIN particularity

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include "main.h"
 
 /**
@@ -8,6 +9,14 @@
 
 void print_number(int n)
 {
+	int lastDigit = 0;
+
+	if (n == INT_MIN)
+	{
+		lastDigit = - (n % 10);
+		n += 1;
+	}
+
 	if (n < 0)
 	{
 		_putchar('-');
@@ -17,5 +26,8 @@ void print_number(int n)
 	if (n / 10)
 		print_number(n / 10);
 
-	_putchar('0' + n % 10);
+	if (lastDigit)
+		_putchar('0' + lastDigit);
+	else
+		_putchar('0' + n % 10);
 }
