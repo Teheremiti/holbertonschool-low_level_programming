4f626111c92d4f70fe9834fa48a7d62d576d6200
Teheremiti <t.tuiaiho@gmail.com>
2023-02-24 02:27:09 -0800

    Clean headers

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,4 +1,3 @@
-#include <time.h>
 #include "main.h"
 
 /**
---
+++ functions_nested_loops/7-main.c
@@ -0,0 +1,21 @@
+#include <limits.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int r;
+
+    print_last_digit(98);
+    print_last_digit(0);
+    r = print_last_digit(-1024);
+    print_last_digit(INT_MAX);
+    print_last_digit(INT_MIN);
+    _putchar('0' + r);
+    _putchar('\n');
+    return (0);
+}
