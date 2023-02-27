3c955ff2aaed59ab59c773da37e53072a11cf62b
Teheremiti <t.tuiaiho@gmail.com>
2023-02-27 05:54:57 -0800

    Print the reverse of a string

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * _strlen - Return the length of a string
+ * @str: Intput string pointer
+ * Return: len
+ */
+
+int _strlen(char *str)
+{
+	int len = 0;
+
+	while (*str != '\0')
+	{
+		len++;
+		str++;
+	}
+
+	return (len);
+}
+
+/**
+ * print_rev - Print a string in reverse
+ * @s: Intput string pointer
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = _strlen(s);
+
+	for (; i >= 0; i--)
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
