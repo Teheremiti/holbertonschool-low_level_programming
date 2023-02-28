a8f037d6784c6ff7a05200216ca5ff1ed17b7e21
Teheremiti <t.tuiaiho@gmail.com>
2023-02-28 01:46:02 -0800

    Own implementation of strcat()

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenate two strings
+ * @src: Input source string pointer
+ * @dest: Input destination string pointer
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		continue;
+
+	for (j = 0; src[j] != '\0'; j++)
+		dest[i + j] = src[j];
+
+	dest[i + j] = '\0';
+	return (dest);
+}
