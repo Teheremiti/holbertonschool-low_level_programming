cd712b3605082b01486532039412a1d7fa45bd23
Teheremiti <t.tuiaiho@gmail.com>
2023-03-01 08:38:21 -0800

    Print chessboard on the terminal

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - Print the chessboard
+ * @a: Input 2Darray in 8x8
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+
+		_putchar('\n');
+	}
+}
