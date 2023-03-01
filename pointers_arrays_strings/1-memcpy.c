aeac79819e3886ac5ac6ee2558461305d8048ceb
Teheremiti <t.tuiaiho@gmail.com>
2023-03-01 05:28:29 -0800

    Own implementation of memcpy()

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -12,10 +12,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	char *p_dest = dest;
 	char *p_src = src;
-	unsigned int i = 0;
 
-	for (; i < n; i++)
-		p_dest[i] = p_src[i];
+	while(n--)
+		*p_dest++ = *p_src++;
 
 	return (dest);
 }
