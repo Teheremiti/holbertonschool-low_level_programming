71c43a323be70b83333d4ff2bfbfeb41b079dbc3
Teheremiti <t.tuiaiho@gmail.com>
2023-03-06 05:54:34 -0800

    Own implementation of pow() using recursion

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Return the x to the power of y
+ * @x: Input int
+ * @y: Input int
+ * Return: Int (x ** y)
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+
+	if (y == 0)
+		return (1);
+
+	x = x * _pow_recursion(x, y - 1);
+
+	return (x);
+}
