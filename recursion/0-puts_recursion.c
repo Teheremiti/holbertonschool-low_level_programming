1a11e413df1be3791f2005fc4859fb151ac81688
Teheremiti <t.tuiaiho@gmail.com>
2023-03-06 02:05:25 -0800

    Own implementation of puts() using recursion

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Print a string using recursion
+ * @s: Input string
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	_putchar(*s);
+	s++;
+
+	if (*s != '\0')
+		_puts_recursion(s);
+
+	else
+		_putchar('\n');
+}
