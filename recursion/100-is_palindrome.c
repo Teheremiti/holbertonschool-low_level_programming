fdb19268e8adfe0be9d9f37dfc7ce5d9110d2a8b
Teheremiti <t.tuiaiho@gmail.com>
2023-03-08 03:51:59 -0800

    Check if a string is a palindrome using recursion

---
+++ recursion/100-is_palindrome.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _palindrome - Check if a string is a palindrome
+ * @s: Input string
+ * Return: 1 if s is a palindrome, otherwise 0
+ */
+
+int _palindrome(char *s, int i)
+{
+	int max_i = strlen(s) - (i + 1);
+
+	if (max_i + 1 <= 1)
+		return (1);
+
+	if (*s != s[max_i])
+		return (0);
+
+	return _palindrome(++s, ++i);
+}
+
+/**
+ * is_palindrome -  Check if a string is a palindrome
+ * @s: Input string
+ * Return: 1 if s is a palindrome, otherwise 0
+ */
+
+int is_palindrome(char *s)
+{
+	return _palindrome(s, 0);
+}
