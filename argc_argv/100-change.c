10fb245e090e27257f67339bf86e01dad38277e9
Teheremiti <t.tuiaiho@gmail.com>
2023-03-08 05:12:27 -0800

    Find the mininum number of coins to make an amount of money

---
+++ argc_argv/100-change.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print the minimum number of coins to make change for
+ * an amount of money
+ *
+ * @argc: Count of the arguments supplied to the program
+ * @argv: Array of pointers to the strings which are those arguments
+ *
+ * Return: 1 on error, 0 on success
+ */
+
+int main(int argc, char **argv)
+{
+	int cents, coins = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	cents = atoi(argv[1]);
+
+	while (cents > 0)
+	{
+		if (!(cents % 25))
+		{
+			cents -= 25;
+			coins += 1;
+		}
+		else if (!(cents % 10))
+		{
+			cents -= 10;
+			coins += 1;
+		}
+		else if (!(cents % 5))
+		{
+			cents -= 5;
+			coins += 1;
+		}
+		else if (!(cents % 2))
+		{
+			cents -= 2;
+			coins += 1;
+		}
+		else if (!(cents % 1))
+		{
+			cents -= 1;
+			coins += 1;
+		}
+	}
+	printf("%d\n", coins);
+	return (0);
+}
