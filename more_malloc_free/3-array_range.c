a6a80c0a9285e65c85afb6a08827ba77d7e1d508
Teheremiti <t.tuiaiho@gmail.com>
2023-03-14 04:03:41 -0700

    Create an array of ints using malloc

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - Create an array of ints using malloc
+ *
+ * @min: Input int
+ * @max: Input int
+ *
+ * Return: NULL if min > max or on FAILURE
+ * A  pointer to the new array on SUCCESS
+ */
+
+int *array_range(int min, int max)
+{
+	int *range, i, len;
+
+	if (min > max)
+		return (NULL);
+
+	len = max - min;
+
+	range = malloc(sizeof(*range) * len + 1);
+
+	if (range == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		range[i] = min + i;
+
+	return (range);
+}
