764ec93f592e0d4a25d1c0b979f8f5d977a0f2b6
Teheremiti <t.tuiaiho@gmail.com>
2023-03-20 01:48:03 -0700

    Add dog structure

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - Structure containing infos about dogs
+ *
+ * @name: Input string, dog name
+ * @age: Input float, dog age
+ * @owner: Input string, dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
