557185386442e327d97e49b1014f336073cb9e7f
Teheremiti <t.tuiaiho@gmail.com>
2023-03-20 03:09:46 -0700

    Create a new dog in struct dog - Store copies with malloc

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,20 +12,43 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int len1 = 0, len2 = 0, i;
+	char *cp_name = NULL, *cp_owner = NULL;
+
 	dog_t *d = malloc(sizeof(*d));
 
 	if (d == NULL)
-		free(d);
-
-	else
 	{
-		char *cp_name = name;
-		char *cp_owner = owner;
-
-		d->name = cp_name;
-		d->age = age;
-		d->owner = cp_owner;
+		free(d);
+		d = NULL;
+		return (NULL);
 	}
+
+	for (; name[len1] != '\0'; len1++)
+	{}
+
+	for (; owner[len2] != '\0'; len2++)
+	{}
+
+	cp_name = malloc(len1 * sizeof(*cp_name));
+	cp_owner = malloc(len2 * sizeof(*cp_owner));
+
+	if (cp_name == NULL || cp_owner == NULL)
+	{
+		free(d), free(cp_name), free(cp_owner);
+		d = NULL, cp_name = NULL, cp_owner = NULL;
+		return (NULL);
+	}
+
+	for (i = 0; name[i] != '\0'; i++)
+		cp_name[i] = name[i];
+
+	for (i = 0; owner[i] != '\0'; i++)
+		cp_owner[i] = owner[i];
+
+	d->name = cp_name;
+	d->age = age;
+	d->owner = cp_owner;
 
 	return (d);
 }
