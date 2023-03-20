7dad73db00eca319f119cd421520f8a74bfcd402
Teheremiti <t.tuiaiho@gmail.com>
2023-03-20 02:31:09 -0700

    Print the elements of the variable of type struct dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,8 +11,17 @@ void print_dog(struct dog *d)
 {
 	if (d != NULL)
 	{
+		if (d->name == NULL)
+			d->name = "(nil)";
 		printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
+
+		if (d->age == 0.0)
+			printf("Age: (nil)\n");
+		else
+			printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+			d->owner = "(nil)";
 		printf("Owner: %s\n", d->owner);
 	}
 }
