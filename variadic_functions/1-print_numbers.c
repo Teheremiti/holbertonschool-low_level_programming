3bedb4c16ac5f5ef0e1ad9189a49fa751c8c8612
Teheremiti <t.tuiaiho@gmail.com>
2023-03-21 12:18:41 -0700

    Print numbers with a specific separator using variadic func

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Print numbers
+ *
+ * @separator: String to be printed between numbers
+ * @n: Number of integers passed
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list ap;
+	unsigned int i;
+
+	va_start(ap, n);
+
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(ap, int));
+
+		if (i != n - 1)
+			printf("%s", separator);
+	}
+
+	va_end(ap);
+	printf("\n");
+}
