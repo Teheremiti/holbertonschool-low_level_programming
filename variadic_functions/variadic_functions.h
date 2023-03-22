8f8dc3115d5b7dede06df7decc896666ea33bb67
Teheremiti <t.tuiaiho@gmail.com>
2023-03-22 06:31:52 -0700

    Add print_type structure

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -9,6 +9,21 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+
+/**
+ * print_type - Struct to find the correct type to print
+ *
+ * @type: Type to print
+ * @f: Function to print with correct type
+ */
+
+typedef struct print_type
+{
+	char *type;
+	void (*f)();
+} print_type_t;
+
 void print_all(const char * const format, ...);
 
 #endif
