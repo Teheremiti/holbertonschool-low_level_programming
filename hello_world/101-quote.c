576b41acc93455100f67b0f0280365dc34290a94
Teheremiti Tuiaiho <120781178+Teheremiti@users.noreply.github.com>
2023-04-01 15:15:14 +0200

    Replice `fprintf` with `write`

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,7 +7,6 @@
 
 int main(void)
 {
-	fprintf(stderr, "%s",
-			 "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
