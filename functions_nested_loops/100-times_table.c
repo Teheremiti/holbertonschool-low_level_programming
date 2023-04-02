b34d49fd0bc8cc4634b2b7c8075e0819ba0c3376
Teheremiti Tuiaiho <120781178+Teheremiti@users.noreply.github.com>
2023-04-02 14:35:44 +0200

    FIX: Betty - more than 40 lines per func

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,4 +1,44 @@
 #include "main.h"
+
+/**
+ * print_single - Print a single digit number with appropriate formating
+ * Return: (void)
+ */
+
+void print_single(int num)
+{
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('0' + num % 10);
+}
+
+/**
+ * print_double - Print a two digits number with appropriate formating
+ * Return: (void)
+ */
+
+void print_double(int num)
+{
+	_putchar(' ');
+	_putchar(' ');
+	_putchar('0' + product / 10);
+	_putchar('0' + product % 10);
+}
+
+/**
+ * print_triple - Print a three digits number with appropriate formating
+ * Return: (void)
+ */
+
+void print_triple(int num)
+{
+	_putchar(' ');
+	_putchar('0' + product / 100);
+	_putchar('0' + (product / 10) % 10);
+	_putchar('0' + product % 10);
+}
+
 /**
  * print_times_table - Print the n times table
  * @n: Input int
@@ -13,7 +53,6 @@ void print_times_table(int n)
 
 	for (; i <= n; i++)
 	{
-		k = 1;
 		_putchar('0');
 
 		if (n != 0)
@@ -24,31 +63,19 @@ void print_times_table(int n)
 			return;
 		}
 
-		for (; k <= n; k++)
+		for (k = 1; k <= n; k++)
 		{
 			product = i * k;
 
 			if (product < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + product % 10);
-			}
+				print_single(product);
+				
 			else if (product >= 10 && product < 100)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + product / 10);
-				_putchar('0' + product % 10);
-			}
+				print_double(product);
+			
 			else
-			{
-				_putchar(' ');
-				_putchar('0' + product / 100);
-				_putchar('0' + (product / 10) % 10);
-				_putchar('0' + product % 10);
-			}
+				print_triple(product);
+			
 			if (k != n)
 				_putchar(',');
 		}
