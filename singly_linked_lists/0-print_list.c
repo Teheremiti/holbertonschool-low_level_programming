c7c7345a6009fd2428b15265aff5a52153580d47
Teheremiti <t.tuiaiho@gmail.com>
2023-04-03 05:14:27 -0700

    Print all the elements of a single linked list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * print_list - Print all the elements of a list_t list
+ *
+ * @h: Pointer to an element of list_t
+ *
+ * Return: The number of nodes
+ * Format: [len] <h->str>
+ */
+
+size_t print_list(const list_t *h)
+{
+	list_t *node = NULL;
+	int count = 0;
+
+	node = malloc(sizeof(list_t));
+	if (node == NULL)
+	{
+		free(node);
+		return (0);
+	}
+
+	node->str = h->str;
+	node->len = h->len;
+	node->next = h->next;
+
+	while (node != NULL)
+	{
+		if (node->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", node->len, node->str);
+
+		node = node->next;
+		count++;
+	}
+	return (count);
+}
