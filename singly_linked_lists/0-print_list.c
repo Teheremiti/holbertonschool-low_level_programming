6f625cf980dc2104e81770100468323d79612f08
Teheremiti <t.tuiaiho@gmail.com>
2023-04-03 07:46:20 -0700

    Use recursiveness to avoid malloc

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,32 +11,18 @@
 
 size_t print_list(const list_t *h)
 {
-	list_t *node = NULL;
-	int count = 0;
+	size_t count = 1;
 
-	node = malloc(sizeof(list_t));
-	if (node == NULL || h == NULL)
-	{
-		free(node);
-		node = NULL;
+	if (h == NULL)
 		return (0);
-	}
 
-	node->str = h->str;
-	node->len = h->len;
-	node->next = h->next;
+	if (h->str == NULL)
+		printf("[0] (nil)\n");
+	else
+		printf("[%u] %s\n", h->len, h->str);
 
-	while (node != NULL)
-	{
-		if (node->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", node->len, node->str);
+	if (h->next != NULL)
+		count += print_list(h->next);
 
-		node = node->next;
-		count++;
-	}
-	free(node);
-	node = NULL;
 	return (count);
 }
