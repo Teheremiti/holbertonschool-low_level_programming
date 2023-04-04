aef0fdbe8e77af94906d16b71c712ef7d44d6aab
Teheremiti <t.tuiaiho@gmail.com>
2023-04-04 05:54:58 -0700

    Handle out of range index case

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,6 +1,29 @@
 #include "lists.h"
 
 /**
+ * dlistint_len - Return the number of nodes in a dlistint list
+ *
+ * @h: Pointer to the head of the list
+ *
+ * Return: The len of the dlistint list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	if (h == NULL)
+		return (0);
+
+	count++;
+	if (h->next != NULL)
+		count += dlistint_len(h->next);
+
+	return (count);
+}
+
+
+/**
  * get_dnodeint_at_index - Get the nth node of a dlitsint_t linked list
  *
  * @head: Pointer to the head of the list
@@ -13,6 +36,10 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *node = head;
 	unsigned int i = 0;
+	size_t len = dlistint_len(head);
+
+	if (index > len)
+		return (NULL);
 
 	if (head == NULL)
 		return (NULL);
