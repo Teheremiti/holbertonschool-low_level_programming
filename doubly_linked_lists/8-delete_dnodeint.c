c448af7c02d618767fae166546c901b5ff66cf59
Teheremiti Tuiaiho <120781178+Teheremiti@users.noreply.github.com>
2023-04-05 21:24:35 +0200

    FIX: NULL case

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -15,7 +15,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *left = NULL;
 	dlistint_t *right = NULL;
 
-	if (head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
 	tmp = *head;
