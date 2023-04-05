40704e32e88fb814a8d3262e765e019020f9c63a
Teheremiti <t.tuiaiho@gmail.com>
2023-04-05 01:17:14 -0700

    FIX: Add at last index case

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,29 +1,6 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Return the number of nodes in a dlistint list
- *
- * @h: Pointer to the head of the list
- *
- * Return: The len of the dlistint list
- */
-
-size_t dlistint_len(const dlistint_t *h)
-{
-	size_t count = 0;
-
-	if (h == NULL)
-		return (0);
-
-	count++;
-	if (h->next != NULL)
-		count += dlistint_len(h->next);
-
-	return (count);
-}
-
-
-/**
  * insert_dnodeint_at_index - Insert a new node at a given position
  *
  * @h: Double pointer to the head of the list
@@ -37,8 +14,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *tmp = *h;
 	dlistint_t *newNode = malloc(sizeof(dlistint_t));
-	unsigned int i = 0;
-	unsigned int len = dlistint_len(*h);
+	unsigned int i = 1;
 
 	if (newNode == NULL)
 	{
@@ -46,17 +22,19 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 
-	if (h == NULL || idx > len)
+	if (h == NULL && idx != 0)
 		return (NULL);
 
 	if (idx == 0)
-		return ((add_dnodeint(h, n)));
+		return (add_dnodeint(h, n));
 
-	else if (idx == len - 1)
-		return (add_dnodeint_end(h, n));
+	for (; i < idx; i++)
+	{
+		if (tmp->next == NULL)
+			return (add_dnodeint_end(h, n));
 
-	for (; i < idx - 1; i++)
 		tmp = tmp->next;
+	}
 
 	newNode->n = n;
 	newNode->prev = tmp;
