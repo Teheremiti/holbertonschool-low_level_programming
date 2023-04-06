a3899de164f879b47ea3ab2f8b0414cb33a3bee6
Teheremiti <t.tuiaiho@gmail.com>
2023-04-06 09:20:02 -0700

    Optimize code using bitwise operators

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -1,51 +1,27 @@
 #include "main.h"
 
 /**
- * powerofTwo - Elevate 2 to the power of a given positive int
- *
- * @i: Integer to raise to the power of
- *
- * Return: 2 to the power i
- */
-
-unsigned int powerofTwo(int i)
-{
-	if (i == 0)
-		return (1);
-
-	if (i == 1)
-		return (2);
-
-	return (2 * powerofTwo(i - 1));
-}
-
-/**
  * binary_to_uint - Convert a binary number to an unsigned int
  *
  * @b: String binary number
  *
- * Return: The converted number on SUCCESS, otherwise 0
+ * Return: (void)
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int decimal = 0;
-	int len = 0, i = 0;
+	unsigned int n = 0;
 
 	if (b == NULL)
 		return (0);
 
-	for (; b[len] != '\0'; len++)
-		if (b[len] != '0' && b[len] != '1')
+	for (; *b != '\0'; b++)
+	{
+		if (*b != '0' && *b != '1')
 			return (0);
 
-	for (; i < len; i++)
-	{
-		int bit = len - i - 1;
-
-		if (b[bit] == '1')
-			decimal += powerofTwo(i);
+		n = (n << 1) + *b - '0';
 	}
 
-	return (decimal);
+	return (n);
 }
