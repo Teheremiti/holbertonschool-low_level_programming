34d5d4d46e21e596def66ce25fc71e6c7b417274
Teheremiti <t.tuiaiho@gmail.com>
2023-04-07 04:30:55 -0700

    Optimize code using recursion

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -10,25 +10,15 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long count = 0;
-	unsigned long mask = 1;
-
 	if (n == 0)
-	{
 		_putchar('0');
-		return;
-	}
 
-	for (; (n >> count) != 0; count++)
-	{}
+	else if (n == 1)
+		_putchar('1');
 
-	mask <<= count - 1;
-
-	for (; mask != 0; mask >>= 1)
+	else
 	{
-		if (n & mask)
-			_putchar('1');
-		else
-			_putchar('0');
+		print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
 }
