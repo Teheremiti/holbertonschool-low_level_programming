875900bfe2aaff857cbce7e4cac5e3c8989efea2
Teheremiti <t.tuiaiho@gmail.com>
2023-04-07 06:20:44 -0700

    Trailing whitesspace

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -15,7 +15,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long differences = n ^ m;
 	int count = 0;
 
-	for (; differences != 0; differences >>=1)
+	for (; differences != 0; differences >>= 1)
 		count += differences & 1;
 
 	return (count);
