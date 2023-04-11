bb831d4a581114a2181ffc4a2843a51f12ed4cb7
Teheremiti <t.tuiaiho@gmail.com>
2023-04-11 05:38:06 -0700

    Create a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * create_file - Create a file
+ *
+ * @filename: Path to the file
+ * @text_content: String to write to the file
+ *
+ * Return: 1 on SUCCESS, otherwise -1
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len, bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+
+	len = strlen(text_content);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1 || bytes_written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
