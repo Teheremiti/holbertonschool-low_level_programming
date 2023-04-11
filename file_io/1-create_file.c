051d74b16a7739af080c717229af58b7c627bc29
Teheremiti <t.tuiaiho@gmail.com>
2023-04-11 05:39:57 -0700

    Handle NULL text_content case

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -24,7 +24,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	len = strlen(text_content);
+	len = (text_content != NULL) ? strlen(text_content) : 0;
 
 	if (text_content != NULL)
 	{
