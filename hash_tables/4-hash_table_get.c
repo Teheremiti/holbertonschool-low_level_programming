c5268631d66017832570fcd7a121de29a85562ac
Teheremiti <t.tuiaiho@gmail.com>
2023-04-13 05:50:28 -0700

    Get the value associated with a key if it exists

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+#include <string.h>
+
+/**
+ * hash_table_get - Get the value associated with a key
+ *
+ * @ht: Hash table to scan
+ * @key: Key to search
+ *
+ * Return: The value associated with the key on SUCCESS, otherwise NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	hash_node_t *tmp;
+	unsigned long index = key_index((unsigned char *)key, ht->size);
+
+	tmp = ht->array[index];
+	while (tmp != NULL)
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+
+	return (NULL);
+}
