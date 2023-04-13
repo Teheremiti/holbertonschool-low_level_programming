b1e4faaced25ff2a9a4dc5c612e8d32750f52167
Teheremiti <t.tuiaiho@gmail.com>
2023-04-13 06:00:10 -0700

    Handle NULL case and collisons

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -13,12 +13,20 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	hash_node_t *tmp;
-	unsigned long index = key_index((unsigned char *)key, ht->size);
+	unsigned long index;
 
-	tmp = ht->array[index];
-	while (tmp != NULL)
-		if (strcmp(tmp->key, key) == 0)
-			return (tmp->value);
+	if (ht != NULL)
+	{
+		index = key_index((unsigned char *)key, ht->size);
+		tmp = ht->array[index];
+		while (tmp != NULL)
+		{
+			if (strcmp(tmp->key, key) == 0)
+				return (tmp->value);
+
+			tmp = tmp->next;
+		}
+	}
 
 	return (NULL);
 }
