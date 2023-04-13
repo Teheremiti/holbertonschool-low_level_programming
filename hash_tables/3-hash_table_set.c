ac4651d0d555cd75f1e4e83b594dcfc2efbd9259
Teheremiti <t.tuiaiho@gmail.com>
2023-04-13 07:30:37 -0700

    Handle memory allocations - free

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,27 +14,19 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long index;
-	char *keyCopy = strdup(key);
-	char *valueCopy = strdup(value);
 	hash_node_t *newNode, *tmp;
 
-	if (ht == NULL || keyCopy == NULL  || valueCopy == NULL)
-	{
-		free(keyCopy), free(valueCopy);
-		return (0);
-	}
-
-	if (*key == '\0' || key == NULL)
+	if (ht == NULL || key == NULL)
 		return (0);
 
-	index = key_index((unsigned char *)keyCopy, ht->size);
+	index = key_index((unsigned char *)key, ht->size);
 	tmp = ht->array[index];
 	while (tmp != NULL)
 	{
-		if (strcmp(tmp->key, keyCopy) == 0)
+		if (strcmp(tmp->key, key) == 0)
 		{
 			free(tmp->value);
-			tmp->value = valueCopy;
+			tmp->value = strdup(value);
 			return (1);
 		}
 		tmp = tmp->next;
@@ -42,13 +34,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	newNode = malloc(sizeof(hash_node_t));
 	if (newNode == NULL)
-	{
-		free(keyCopy), free(valueCopy);
 		return (0);
-	}
 
-	newNode->key = keyCopy;
-	newNode->value = valueCopy;
+	newNode->key = strdup(key);
+	newNode->value = strdup(value);
 
 	newNode->next = ht->array[index];
 	ht->array[index] = newNode;
